aba92e40bbb7047be65847f4c61364799014a7ae
Auraofdivinity <peirisasel373@gmail.com>
2020-02-18 00:12:38 +0530

    Minor changes

--- src/app.js
+++ src/app.js
@@ -2,14 +2,13 @@
 import React, { Component } from "react";
 import MapGL from "react-map-gl";
 import { MapStylePicker } from "./controls";
-import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
-import NativeSelect from "@material-ui/core/NativeSelect";
 import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import FormHelperText from "@material-ui/core/FormHelperText";
 import Select from "@material-ui/core/Select";
-import Typography from "@material-ui/core/Typography";
+import MenuItem from "@material-ui/core/MenuItem";
+
+// Easing function
+import d3 from "d3-ease";
 
 export default class App extends Component {
   state = {
@@ -21,12 +20,15 @@ export default class App extends Component {
       latitude: 51.9607,
       zoom: 11,
       maxZoom: 16
-    }
+    },
+    recentTracks: [],
+    selectedTrack: ""
   };
 
   componentDidMount() {
     window.addEventListener("resize", this._resize);
     this._resize();
+    this.fetchRecentTracks();
   }
 
   componentWillUnmount() {
@@ -50,13 +52,62 @@ export default class App extends Component {
     });
   };
 
-  handleChange = () => {};
+  fetchRecentTracks = () => {
+    let dataArr = [];
+    fetch("https://envirocar.org/auth-proxy/api/tracks/?limit=20", {
+      method: "GET",
+      headers: {
+        Authorization: "Basic QXNlbFBlaXJpczpJc2lwYXRoYW5hOQ=="
+      }
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(resData => {
+        this.setState({ recentTracks: resData.tracks });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  fetchMeasurementsOfSelectedTrack = () => {
+    fetch(
+      "https://envirocar.org/auth-proxy/api/tracks/" +
+        this.state.selectedTrack +
+        "/measurements",
+      {
+        method: "GET",
+        headers: {
+          Authorization: "Basic QXNlbFBlaXJpczpJc2lwYXRoYW5hOQ=="
+        }
+      }
+    )
+      .then(res => {
+        return res.json();
+      })
+      .then(data => console.log(data))
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  handleChange = event => {
+    this.setState(
+      {
+        selectedTrack: event.target.value
+      },
+      () => {
+        this.fetchMeasurementsOfSelectedTrack();
+      }
+    );
+  };
 
   render() {
+    const recentTracks = { ...this.props };
     return (
       <div>
-        <Grid container spacing={10}>
-          <Paper color="red"></Paper>
+        <Grid container>
           <Grid item lg={9} sm={9} md={9}>
             <MapStylePicker
               onStyleChange={this.onStyleChange}
@@ -72,24 +123,29 @@ export default class App extends Component {
             ></MapGL>
           </Grid>
           <Grid item lg={3} sm={3} md={3}>
-            <div style={{ fontFamily: "Montserrat", fontSize: "1.3" }}>
-              The most recent 20 tracks are available in the follwoing dropdown.
+            <div style={{ margin: "20px" }}>
+              <div style={{ fontFamily: "Montserrat", fontSize: "1.4" }}>
+                The most recent 20 tracks are available in the follwoing
+                dropdown.
+              </div>
+              <FormControl variant="outlined" style={{ width: "100%" }}>
+                <Select
+                  onChange={this.handleChange}
+                  style={{ fontFamily: "Montserrat" }}
+                  labelId="demo-simple-select-outlined-label"
+                  id="demo-simple-select-outlined"
+                  value={this.state.selectedTrack}
+                >
+                  {this.state.recentTracks.map((current, index) => {
+                    return (
+                      <MenuItem value={current.id} key={index}>
+                        {current.id}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
+              </FormControl>
             </div>
-            <FormControl variant="outlined" style={{ width: "100%" }}>
-              <Select
-                native
-                onChange={this.handleChange("age")}
-                inputProps={{
-                  name: "age",
-                  id: "outlined-age-native-simple"
-                }}
-              >
-                <option value="" />
-                <option value={10}>Ten</option>
-                <option value={20}>Twenty</option>
-                <option value={30}>Thirty</option>
-              </Select>
-            </FormControl>
           </Grid>
         </Grid>
       </div>
